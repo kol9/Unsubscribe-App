3f90db71ea5fc8237f5a02756c8a35513fc4b346
Nikolay Yarlychenko <yarlychenkonikolay@gmail.com>
2020-03-01 13:25:56 +0300

    Initial Commit

---
+++ Unsubscribe App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unsubscribe App
+//
+//  Created by Nikolay Yarlychenko on 01.03.2020.
+//  Copyright © 2020 Nikolay Yarlychenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
